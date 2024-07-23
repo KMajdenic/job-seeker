50c07def087eeb2e31f8aa55247051cb6f30e932
Karlo Majdenic <karlo.majdenic1@gmail.com>
2024-07-23 19:04:14 +0200

    Proper setup of tailwind and some styles with other various fixes

--- JobSeeker/app/models/tag.rb
+++ JobSeeker/app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ApplicationRecord
   has_many :classfield_tags
   has_and_belongs_to_many :classfields, through: :classfield_tags
+  validates :name, presence: true, uniqueness:true
 end
